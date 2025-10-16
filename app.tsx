6432efb5dd8b0a9e1dbafd913236b5c94784c491
Stems <70470779+satoshistackalotto@users.noreply.github.com>
2025-10-16 10:10:30 +0300

    Create root App component for Village Wallet
    Signed-off-by: Stems <70470779+satoshistackalotto@users.noreply.github.com>

---
+++ app.tsx
@@ -0,0 +1,11 @@
+/**
+ * Village Wallet - Root App.tsx
+ * This file imports your component from /src
+ */
+
+import React from 'react';
+import VillageWallet from './src/village-wallet-1';
+
+export default function App() {
+  return <VillageWallet />;
+}
